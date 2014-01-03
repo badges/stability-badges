784d614c9aad03b8edb82b42150cae8973e7767b
Hugh Kennedy <hughskennedy@gmail.com>
2014-01-04 00:06:45 +1100

    Update font-family

--- generate.js
+++ generate.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 var header = [
     '<svg width="120" height="19" xmlns="http://www.w3.org/2000/svg">'
   , '<style type="text/css">',
-  , 'text{alignment-baseline:middle;font-family:Arial,Helvetica,sans-serif;font-size:10px}'
+  , 'text{alignment-baseline:middle;font-family:Arial,sans-serif;font-size:10px}'
   , '</style>'
 ].join('')
 var footer = '</svg>'
